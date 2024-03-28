a72c37d08264b966bcad48a71bbb51ac555d8a60
Ravishankar Kushwah <ravishankark.bca2021@ssism.org>
2024-03-28 19:43:28 +0530

    HMS

---
+++ dashboard/src/components/Login.jsx
@@ -0,0 +1,85 @@
+import React, { useContext, useState } from "react";
+import { Navigate, useNavigate } from "react-router-dom";
+import { toast } from "react-toastify";
+import { Context } from "../main";
+import axios from "axios";
+
+const Login = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
+
+  const { isAuthenticated, setIsAuthenticated } = useContext(Context);
+
+  const navigateTo = useNavigate();
+
+  const [loading, setLoading] = useState(false); // State variable to track loading state
+  const handleLogin = async (e) => {
+    e.preventDefault();
+    setLoading(true); // Set loading state to true
+    try {
+      await axios
+        .post(
+          "http://localhost:4000/api/v1/user/login",
+          { email, password, confirmPassword, role: "Admin" },
+          {
+            withCredentials: true,
+            headers: { "Content-Type": "application/json" },
+          }
+        )
+        .then((res) => {
+          toast.success(res.data.message);
+          setIsAuthenticated(true);
+          navigateTo("/");
+          setEmail("");
+          setPassword("");
+          setConfirmPassword("");
+        });
+    } catch (error) {
+      toast.error(error.response.data.message);
+    } finally {
+      setLoading(false); // Set loading state to false regardless of success or failure
+    }
+  };
+
+  if (isAuthenticated) {
+    return <Navigate to={"/"} />;
+  }
+
+  return (
+    <>
+      <section className="container form-component">
+        <img src="/logo.png" alt="logo" className="logo" />
+        <h1 className="form-title">WELCOME TO ZEECARE</h1>
+        <p>Only Admins Are Allowed To Access These Resources!</p>
+        <form onSubmit={handleLogin}>
+          <input
+            type="text"
+            placeholder="Email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+          <input
+            type="password"
+            placeholder="Password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+          <input
+            type="password"
+            placeholder="Confirm Password"
+            value={confirmPassword}
+            onChange={(e) => setConfirmPassword(e.target.value)}
+          />
+          <div style={{ justifyContent: "center", alignItems: "center" }}>
+            <button type="submit" disabled={loading}>
+            {loading ? "Loading..." : "Login"}
+            </button>
+          </div>
+        </form>
+      </section>
+    </>
+  );
+};
+
+export default Login;
---
+++ frontend/src/components/MessageForm.jsx
@@ -0,0 +1,92 @@
+import axios from "axios";
+import React, { useState } from "react";
+import { toast } from "react-toastify";
+
+const MessageForm = () => {
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
+  const [email, setEmail] = useState("");
+  const [phone, setPhone] = useState("");
+  const [message, setMessage] = useState("");
+
+  const [loading, setLoading] = useState(false); // State variable to track loading state
+  const handleMessage = async (e) => {
+    e.preventDefault();
+    setLoading(true); // Set loading state to true
+    try {
+      await axios
+        .post(
+          "http://localhost:4000/api/v1/message/send",
+          { firstName, lastName, email, phone, message },
+          {
+            withCredentials: true,
+            headers: { "Content-Type": "application/json" },
+          }
+        )
+        .then((res) => {
+          toast.success(res.data.message);
+          setFirstName("");
+          setLastName("");
+          setEmail("");
+          setPhone("");
+          setMessage("");
+        });
+    } catch (error) {
+      toast.error(error.response.data.message);
+    } finally {
+      setLoading(false); // Set loading state to false regardless of success or failure
+    }
+  };
+
+  return (
+    <>
+      <div className="container form-component message-form">
+        <h2>Send Us A Message</h2>
+        <form onSubmit={handleMessage}>
+          <div>
+            <input
+              type="text"
+              placeholder="First Name"
+              value={firstName}
+              onChange={(e) => setFirstName(e.target.value)}
+            />
+            <input
+              type="text"
+              placeholder="Last Name"
+              value={lastName}
+              onChange={(e) => setLastName(e.target.value)}
+            />
+          </div>
+          <div>
+            <input
+              type="text"
+              placeholder="Email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+            />
+            <input
+              type="number"
+              placeholder="Mobile Number"
+              value={phone}
+              onChange={(e) => setPhone(e.target.value)}
+            />
+          </div>
+          <textarea
+            rows={7}
+            placeholder="Message"
+            value={message}
+            onChange={(e) => setMessage(e.target.value)}
+          />
+          <div style={{ justifyContent: "center", alignItems: "center" }}>
+            <button type="submit" disabled={loading}>
+            {loading ? "Loading..." : "Send"}
+            </button>
+          </div>
+        </form>
+        <img src="/Vector.png" alt="vector" />
+      </div>
+    </>
+  );
+};
+
+export default MessageForm;
