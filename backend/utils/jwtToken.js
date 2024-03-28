bca572921cd0821ecd82cef135b498ed76b0c754
Ravishankar Kushwah <110323128+ravishankar-03@users.noreply.github.com>
2024-03-29 01:51:38 +0530

    Update jwtToken.js http

--- backend/utils/jwtToken.js
+++ backend/utils/jwtToken.js
@@ -9,7 +9,7 @@ export const generateToken = (user, message, statusCode, res) => {
       expires: new Date(
         Date.now() + process.env.COOKIE_EXPIRE * 24 * 60 * 60 * 1000
       ),
-      httpOnly: false,
+      httpOnly: true,
       sameSite: 'None',
       secure: true,
     })
