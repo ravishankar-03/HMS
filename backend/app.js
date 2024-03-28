dd117bcc72d6777c962e39e5d50399c03b7f20b2
Ravishankar Kushwah <ravishankark.bca2021@ssism.org>
2024-03-28 21:58:11 +0530

    message hello bro in backend

--- backend/app.js
+++ backend/app.js
@@ -34,6 +34,13 @@ app.use("/api/v1/message", messageRouter);
 app.use("/api/v1/user", userRouter);
 app.use("/api/v1/appointment", appointmentRouter);
 
+app.get("/", (req, res, next)=>{
+  return res.status(200).json({
+  success: true,
+  message: "Hello Bro",
+});
+});
+
 dbConnection();
 
 app.use(errorMiddleware);
